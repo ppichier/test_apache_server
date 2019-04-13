4bb523754bde064c8b9d108949f2c12c6b0ecd1b
Pier'Antonio <pierantonio.pichierri@gmail.com>
2019-04-13 12:13:37 +0200

    [ADD] dist folder first try

---
+++ precache-manifest.622765d702e8ca19afee47917f458afc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "38f4ff01e1d981f1ec1a",
+    "url": "/css/app.118230d7.css"
+  },
+  {
+    "revision": "38f4ff01e1d981f1ec1a",
+    "url": "/js/app.7bfafad3.js"
+  },
+  {
+    "revision": "88f99586ae4e3632936d",
+    "url": "/css/chunk-vendors.d91ccead.css"
+  },
+  {
+    "revision": "88f99586ae4e3632936d",
+    "url": "/js/chunk-vendors.e8528e54.js"
+  },
+  {
+    "revision": "528370d92ea341c59aaa6a7fb0fd8a68",
+    "url": "/media/songCall.528370d9.mp3"
+  },
+  {
+    "revision": "cfdfdaec04988528c736d0096af6f326",
+    "url": "/media/songCloseCall.cfdfdaec.mp3"
+  },
+  {
+    "revision": "c9c6fcda3674bb06217d4f5147cc9192",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
